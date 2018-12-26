730bf04962f981f94a6b13f0e2656a83a80b9273
Javi Pérez <xapega@hotmail.es>
2018-12-26 22:21:38 +0100

    Inicio del proyecto.

---
+++ app/src/main/java/com/example/javipercas/endprojectifp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.javipercas.endprojectifp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btSignIn, btLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btSignIn = (Button)findViewById(R.id.btSignIn);
+        btLogin = (Button)findViewById(R.id.btLogin);
+
+        btSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(getApplicationContext(), SignIn.class);
+                startActivity(signIn);
+            }
+        });
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent help = new Intent(getApplicationContext(), Help.class);
+                startActivity(help);
+            }
+        });
+
+
+    }
+}
